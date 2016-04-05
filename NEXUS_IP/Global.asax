f2ce4566c3ab89661d9b17b48bc84381cb92e2b7
Marcelo Lorenzo Garay <chapattin@hotmail.com>
2016-04-05 17:30:09 -0300

    Prueba puesta en marcha de repositorio - 4/05/2016

---
+++ NEXUS_IP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NEXUS_IP.MvcApplication" Language="C#" %>
---
+++ NEXUS_IP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NEXUS_IP.Startup))]
+namespace NEXUS_IP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
